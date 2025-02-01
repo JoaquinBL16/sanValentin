8a9723e72046b6385934aff1b001dcbd789108fb
JoaquinBL16 <84550387+JoaquinBL16@users.noreply.github.com>
2025-02-01 20:03:05 -0300

    Update script.js

--- Propuestajavi/script.js
+++ Propuestajavi/script.js
@@ -1,6 +1,7 @@
 const yesBtn = document.getElementById("yes-btn")
 const noBtn = document.getElementById("no-btn")
 const backgroundImage = document.getElementById("background-image")
+const container = document.querySelector(".container")
 
 const images = ["imagenes/minion1.jpg", "imagenes/minion2.jpg", "imagenes/imagen1.jpg", "imagenes/imagen2.jpg"]
 let currentImageIndex = 0
@@ -14,10 +15,9 @@ yesBtn.addEventListener("click", () => {
   alert("¡Sabía que dirías que sí! Te amo con todo mi corazón ❤️\n\nFeliz San Valentín")
 })
 
-noBtn.addEventListener("mouseover", (event) => {
-  const container = document.querySelector(".container")
-  const buttonRect = noBtn.getBoundingClientRect()
+function moveButton(e) {
   const containerRect = container.getBoundingClientRect()
+  const buttonRect = noBtn.getBoundingClientRect()
 
   const maxX = containerRect.width - buttonRect.width
   const maxY = containerRect.height - buttonRect.height
@@ -30,6 +30,12 @@ noBtn.addEventListener("mouseover", (event) => {
   noBtn.style.top = `${randomY}px`
 
   changeBackgroundImage()
+}
+
+noBtn.addEventListener("mouseover", moveButton)
+noBtn.addEventListener("touchstart", (e) => {
+  e.preventDefault() // Previene el comportamiento por defecto del toque
+  moveButton(e)
 })
 
 // Precargar imágenes
